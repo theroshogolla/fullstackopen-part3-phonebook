f07a90349b5221727f03ead2cdc639fdd1d520fa
Rounak Chawla <ron.chawla9@gmail.com>
2020-12-27 01:52:36 +0530

    Frontend production build

---
+++ build/precache-manifest.d856bd070772fba36990f661bc533602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cea3a09ae8fe752ec386dbc344f230f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa356a675dbfe0bba9ab",
+    "url": "/static/js/2.4541878e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4541878e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d390857bb6328d619679",
+    "url": "/static/js/main.065dae54.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
